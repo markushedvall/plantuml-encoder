f5ef214cc0d2c44911ac7b1b9e13a6b8c3619d19
Markus Hedvall <mackanhedvall@gmail.com>
2019-08-07 18:07:24 +0200

    Add tests of browser code

--- test/plantuml-encoder-test.js
+++ test/plantuml-encoder-test.js
@@ -1,17 +1,29 @@
 /* global describe it */
 var chai = require('chai')
-var plantumlEncoder = require('../lib/plantuml-encoder')
+var nodeEncoder = require('../lib/plantuml-encoder')
+var browserEncoder = require('../dist/plantuml-encoder')
+var minifiedEncoder = require('../dist/plantuml-encoder.min')
 
 var expect = chai.expect
 
 describe('plantuml-encoder', function () {
-  describe('#encode()', function () {
-    it('should encode "A -> B: Hello"', function () {
-      var encoded = plantumlEncoder.encode('A -> B: Hello')
-      expect(encoded).to.equal('SrJGjLDmibBmICt9oGS0')
-    })
+  describe('node.js', function () {
     it('should encode UTF-8 "A -> B: Hello/你好"', function () {
-      var encoded = plantumlEncoder.encode('A -> B: Hello/你好')
+      var encoded = nodeEncoder.encode('A -> B: Hello/你好')
+      expect(encoded).to.equal('SrJGjLDmibBmICt9oTT_idV1qwLx0G00')
+    })
+  })
+
+  describe('browser', function () {
+    it('should encode UTF-8 "A -> B: Hello/你好"', function () {
+      var encoded = browserEncoder.encode('A -> B: Hello/你好')
+      expect(encoded).to.equal('SrJGjLDmibBmICt9oTT_idV1qwLx0G00')
+    })
+  })
+
+  describe('browser with minified code', function () {
+    it('should encode UTF-8 "A -> B: Hello/你好"', function () {
+      var encoded = minifiedEncoder.encode('A -> B: Hello/你好')
       expect(encoded).to.equal('SrJGjLDmibBmICt9oTT_idV1qwLx0G00')
     })
   })
