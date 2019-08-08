e1270ed60d6d534a8a49c1af484d03e333606c09
Markus Hedvall <mackanhedvall@gmail.com>
2019-08-08 15:03:25 +0200

    Remove usage of utf8-bytes

--- dist/plantuml-encoder.js
+++ dist/plantuml-encoder.js
@@ -61,17 +61,15 @@ module.exports = function (data) {
 },{}],2:[function(require,module,exports){
 'use strict'
 
-var utf8bytes = require('utf8-bytes')
 var pako = require('pako/lib/deflate.js')
 var encode64 = require('./encode64')
 
 module.exports.encode = function (text) {
-  var data = utf8bytes(text)
-  var deflated = pako.deflateRaw(data, { level: 9, to: 'string' })
+  var deflated = pako.deflateRaw(text, { level: 9, to: 'string' })
   return encode64(deflated)
 }
 
-},{"./encode64":1,"pako/lib/deflate.js":3,"utf8-bytes":12}],3:[function(require,module,exports){
+},{"./encode64":1,"pako/lib/deflate.js":3}],3:[function(require,module,exports){
 'use strict';
 
 
@@ -3947,41 +3945,6 @@ function ZStream() {
 }
 
 module.exports = ZStream;
-
-},{}],12:[function(require,module,exports){
-module.exports = function (str) {
-    var bytes = [];
-    for (var i = 0; i < str.length; i++) {
-        var c = str.charCodeAt(i);
-        if (c >= 0xd800 && c <= 0xdbff && i + 1 < str.length) {
-            var cn = str.charCodeAt(i + 1);
-            if (cn >= 0xdc00 && cn <= 0xdfff) {
-                var pt = (c - 0xd800) * 0x400 + cn - 0xdc00 + 0x10000;
-                
-                bytes.push(
-                    0xf0 + Math.floor(pt / 64 / 64 / 64),
-                    0x80 + Math.floor(pt / 64 / 64) % 64,
-                    0x80 + Math.floor(pt / 64) % 64,
-                    0x80 + pt % 64
-                );
-                i += 1;
-                continue;
-            }
-        }
-        if (c >= 2048) {
-            bytes.push(
-                0xe0 + Math.floor(c / 64 / 64),
-                0x80 + Math.floor(c / 64) % 64,
-                0x80 + c % 64
-            );
-        }
-        else if (c >= 128) {
-            bytes.push(0xc0 + Math.floor(c / 64), 0x80 + c % 64);
-        }
-        else bytes.push(c);
-    }
-    return bytes;
-};
 
 },{}]},{},[2])(2)
 });
